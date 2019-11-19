a8badf75151b33dc0c6719b19abec9c11f9ac54f
github username <github email address>
2019-11-19 00:35:01 -0600

    added logout and ascify

--- lib/command.rb
+++ lib/command.rb
@@ -1,16 +1,14 @@
+
+
 class CommandLineInterface 
     
     def run 
         self.welcome
-        # self.login
-        # book_query = get_book_query_from_user
-        # book_hash = get_books_from_api(book_query)
-        # display_books(book_hash)
     end 
 
     def welcome 
         puts "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
-        puts "Welcome to BookShelf! Happy Reading!"
+        puts ascii.asciify("Welcome to BookShelf!")
         puts "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
         self.login
     end 
@@ -19,22 +17,37 @@ class CommandLineInterface
         puts "Please enter your username"
         username = gets.chomp
         @user = User.find_or_create_by(username: username)
+        sleep(1)
         puts `clear`
         self.personal_welcome 
     end 
 
     def personal_welcome
-        puts "--------------Hello there #{@user.username}--------------"
-        self.main_menu
+        puts ascii.asciify("Hello #{@user.username}")
+        self.menu_run
     end 
 
     def main_menu
         puts "--------Main Menu---------"
-        puts "Select an option:"
-        puts "1.) Search for books"
-        puts "2.) View book list"
-        puts "3.) Logout"
-        choice = gets.chomp
+        prompt.select("Please select an option", ["Search for books", "View your bookshelf", "Logout"])
+    end 
+    
+    def menu_run
+        menu_choice = self.main_menu
+        case menu_choice
+        when "Search for books"
+            self.get_book_query_from_user
+        when "View your bookshelf"
+            p "------at that list you havent made yet"
+        when "Logout"
+            self.goodbye
+        end 
+    end 
+
+    def goodbye
+        puts `clear`
+        puts ascii.asciify("See you soon")
+        puts ascii.asciify("Happy Reading!!")
     end 
 
     def get_book_query_from_user
@@ -46,5 +59,15 @@ class CommandLineInterface
         book_hash.each.with_index(1) do |data, index|
           puts "#{index}. " + data['title']
         end
-      end
+    end
+
+    def prompt
+        TTY::Prompt.new
+    end 
+
+    def ascii
+        Artii::Base.new
+    end
+
+    
 end 
