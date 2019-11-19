38a76976de114355b36186d3e5e5599f6ba279b1
github username <github email address>
2019-11-18 23:58:17 -0600

    menu options

--- lib/command.rb
+++ lib/command.rb
@@ -3,29 +3,39 @@ class CommandLineInterface
     def run 
         self.welcome
         # self.login
-        book_query = get_book_query_from_user
-        book_hash = get_books_from_api(book_query)
-        display_books(book_hash)
+        # book_query = get_book_query_from_user
+        # book_hash = get_books_from_api(book_query)
+        # display_books(book_hash)
     end 
 
     def welcome 
         puts "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
         puts "Welcome to BookShelf! Happy Reading!"
         puts "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
-        # self.login
+        self.login
     end 
 
-    # def login 
-    #     puts "Please enter your username"
-    #     username = gets.chomp
-    #     @user = User.find_or_create_by(username: username)
-    #     self.printing 
-    # end 
+    def login 
+        puts "Please enter your username"
+        username = gets.chomp
+        @user = User.find_or_create_by(username: username)
+        puts `clear`
+        self.personal_welcome 
+    end 
 
-    # def printing 
-    #     puts "--------------Hello there #{@user.username}--------------"
-        
-    # end 
+    def personal_welcome
+        puts "--------------Hello there #{@user.username}--------------"
+        self.main_menu
+    end 
+
+    def main_menu
+        puts "--------Main Menu---------"
+        puts "Select an option:"
+        puts "1.) Search for books"
+        puts "2.) View book list"
+        puts "3.) Logout"
+        choice = gets.chomp
+    end 
 
     def get_book_query_from_user
         puts "---------Please enter a keyword to search for books ---------"
