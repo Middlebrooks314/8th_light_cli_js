650311c44b30b83787209ba6f18d02c2a3632861
github username <github email address>
2019-11-18 23:45:37 -0600

    started book fetch

--- lib/command.rb
+++ lib/command.rb
@@ -2,24 +2,39 @@ class CommandLineInterface
     
     def run 
         self.welcome
-        self.login
+        # self.login
+        book_query = get_book_query_from_user
+        book_hash = get_books_from_api(book_query)
+        display_books(book_hash)
     end 
 
     def welcome 
         puts "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
         puts "Welcome to BookShelf! Happy Reading!"
         puts "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
-        self.login
+        # self.login
     end 
 
-    def login 
-        puts "Please enter your username"
-        username = gets.chomp
-        @user = User.find_or_create_by(username: username)
-        self.printing 
+    # def login 
+    #     puts "Please enter your username"
+    #     username = gets.chomp
+    #     @user = User.find_or_create_by(username: username)
+    #     self.printing 
+    # end 
+
+    # def printing 
+    #     puts "--------------Hello there #{@user.username}--------------"
+        
+    # end 
+
+    def get_book_query_from_user
+        puts "---------Please enter a keyword to search for books ---------"
+        gets.chomp
     end 
 
-    def printing 
-        puts "--------------Hello there #{@user.username}--------------"   
-    end 
+    def display_books(book_hash)
+        book_hash.each.with_index(1) do |data, index|
+          puts "#{index}. " + data['title']
+        end
+      end
 end 
